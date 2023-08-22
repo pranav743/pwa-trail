d96f9ee0003d3887415c12bd1cf2c2135e89f9d5
Pranav Patil <pranav07@somaiya.edu>
2023-08-22 17:21:39 +0530

    added request for testing

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 function App() {
+
+  const [data,setData] =useState(false);
+  const getData = async () => {
+    try {
+      const res = await axios.get("https://crud-app-three-blond.vercel.app/api/data");
+      console.log(res.data)
+      setData(res)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      { data &&
+        <p style={{textAlign: 'center'}}>{JSON.stringify(data)}</p>
+      }
+      <button onClick={getData} style={{height: '40px', width: '200px', backgroundColor: '#000', color: '#fff'}}>Get Data</button>
     </div>
   );
 }
