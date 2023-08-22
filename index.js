41dbb2cf7ce481f748ebd278686b2af64124038b
Pranav Patil <pranav07@somaiya.edu>
2023-08-22 16:35:45 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const PORT = 5000;
+const cors = require('cors')
+
+
+app.use(cors({
+    origin: ['https://vercel.com/pranav743/crud-app'],
+    methods: ["POST", "GET"],
+    credentials: true
+}));
+app.get('/api/data', (req, res) => {
+  const jsonData = {
+    message: 'Hello from the server!',
+    data: [
+      { id: 1, name: 'Item 1' },
+      { id: 2, name: 'Item 2' }
+    ]
+  };
+
+  res.json(jsonData);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
